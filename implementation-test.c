65e12e48c30293eb04e9adefca32c161712fe775
ArunTSetty <arun.t.shetty@gmail.com>
2016-07-17 20:53:24 -0400

    ONEplusONE-Success

---
+++ implementation-test.c
@@ -0,0 +1,53 @@
+#include <check.h>
+#include "implementation.h"
+#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<check.h>
+#define MAX 4096
+
+
+
+START_TEST(ONEplusONE)
+{	
+	char ostring1[MAX] = "I";
+	char ostring2[MAX] = "I";
+	ck_assert_str_eq(add(ostring1,ostring2), "II");
+	
+
+	
+}
+END_TEST
+
+
+Suite * roman_suite(void)
+{
+    Suite *s;
+    TCase *tc_core;
+
+    s = suite_create("Roman");
+
+    /* Core test case */
+    tc_core = tcase_create("Core");
+
+    tcase_add_test(tc_core, ONEplusONE);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+
+ int main(void)
+ {
+
+    int number_failed;
+    Suite *s;
+    SRunner *sr;
+	
+    s = roman_suite();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+ }
